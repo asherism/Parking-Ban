a40a5044e8527982e9c677f9a8c5f8a1eedfc039
Yashar <yashar@bravenew.world>
2017-02-08 18:42:16 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,45 @@
+// initialize Express in project
+const express = require('express');
+const app = express();
+
+const request = require("request");
+const cheerio = require("cheerio");
+
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+
+
+    request('http://www.halifax.ca/snow/update.php', function (error, response, body) {
+        var $ = cheerio.load(body);
+
+        $('td[width="590"][valign="top"][bgcolor="#CCCCCC"]').each(function (index) {
+
+            if (index == 1) {
+                var status = $(this).text().trim().replace('Status:','');
+
+                let antwoord = "";
+
+                if (status.includes("lifted")) {
+                    antwoord = "No."
+                } else { antwoord = "Yes." }
+
+                res.render('pages/index', { status, antwoord });
+
+            }
+
+        });
+    })
+});
+
+
+app.use(express.static('public'));
+
+// start Express on port 8080
+app.listen(8080, () => {
+    console.log('Server Started on http://localhost:8080');
+    console.log('Press CTRL + C to stop server');
+})
+
+
