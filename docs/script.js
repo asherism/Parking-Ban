5ac0fabfba47f13230ac64f86507bff57fd256d5
Yashar <yashar@bravenew.world>
2017-02-08 20:26:20 -0500

    test

--- docs/script.js
+++ docs/script.js
@@ -6,24 +6,25 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 
-    request('http://www.halifax.ca/snow/update.php', function (error, response, body) {
-        var $ = cheerio.load(body);
+request('http://www.halifax.ca/snow/update.php', function (error, response, body) {
+    var $ = cheerio.load(body);
 
-        $('td[width="590"][valign="top"][bgcolor="#CCCCCC"]').each(function (index) {
+    $('td[width="590"][valign="top"][bgcolor="#CCCCCC"]').each(function (index) {
 
-            if (index == 1) {
-                var status = $(this).text().trim().replace('Status:','');
+        if (index == 1) {
+            var status = $(this).text().trim().replace('Status:', '');
+            $(".status").append(status);
+            
+            let antwoord = "";
 
-                let antwoord = "";
+            if (status.includes("lifted")) {
+                $(".answer").append("No.")
+            } else { $(".answer").append("Yes.") }
 
-                if (status.includes("lifted")) {
-                    antwoord = "No."
-                } else { antwoord = "Yes." }
 
-                res.render('index', { status, antwoord });
 
-            }
+        }
 
-        });
-    })
-});
+    });
+})
+
